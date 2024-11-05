47991f7a4c11c79857f6466059be893a3ff541a5
Raoul de Jonge <rdejonge@viktor.ai>
2024-11-05 15:53:44 +0100

    update to latest viktor sdk

--- app.py
+++ app.py
@@ -4,21 +4,17 @@ import warnings
 warnings.filterwarnings("ignore", category=UserWarning)
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-from viktor import ViktorController
-from viktor.core import File
-from viktor.parametrization import NumberField, ViktorParametrization
-from viktor.views import GeometryResult, GeometryView, ImageResult, ImageView
+import viktor as vkt
 
-# OpenCascade library
 import cadquery as cq
 
-class Parametrization(ViktorParametrization):
-    length = NumberField("Length", default=30, suffix="mm")
-    height = NumberField("Height", default=40, suffix="mm")
-    bearing_diam = NumberField("Bearing diameter", default=22, suffix="mm")
-    thickness = NumberField("Thickness", default=10, suffix="mm")
-    padding = NumberField("Padding", default=8, suffix="mm")
-    fillet = NumberField("Fillet", default=2, num_decimals=1, step=0.5, suffix="mm")
+class Parametrization(vkt.ViktorParametrization):
+    length = vkt.NumberField("Length", default=30, suffix="mm")
+    height = vkt.NumberField("Height", default=40, suffix="mm")
+    bearing_diam = vkt.NumberField("Bearing diameter", default=22, suffix="mm")
+    thickness = vkt.NumberField("Thickness", default=10, suffix="mm")
+    padding = vkt.NumberField("Padding", default=8, suffix="mm")
+    fillet = vkt.NumberField("Fillet", default=2, num_decimals=1, step=0.5, suffix="mm")
 
 
 def generate_assembly(params):
@@ -55,11 +51,11 @@ def generate_assembly(params):
     return assy, body
 
 
-class Controller(ViktorController):
+class Controller(vkt.ViktorController):
     label = "pillow block"
     parametrization = Parametrization
 
-    @GeometryView("Geometry", duration_guess=1, x_axis_to_right=True)
+    @vkt.GeometryView("Geometry", duration_guess=1, x_axis_to_right=True)
     def get_geometry_view(self, params, **kwargs):
         """
         Renders the 3D model. First it (re-)generates the assembly. Then,
@@ -67,11 +63,11 @@ class Controller(ViktorController):
         GeometryResult and displayed as a GeometryView.
         """
         assy, _ = generate_assembly(params)
-        glb = File()  # temporary file to store 3D model as gltf data
+        glb = vkt.File()  # temporary file to store 3D model as gltf data
         cq.occ_impl.exporters.assembly.exportGLTF(assy, glb.source, True)
-        return GeometryResult(geometry=glb)
+        return vkt.GeometryResult(geometry=glb)
 
-    @ImageView("Drawing", duration_guess=1)
+    @vkt.ImageView("Drawing", duration_guess=1)
     def create_result(self, params, **kwargs):
         """
         Renders the 2D schematic. First it (re-)generates the assembly. Then,
@@ -79,6 +75,6 @@ class Controller(ViktorController):
         ImageResult and displayed as an ImageView.
         """
         _, body = generate_assembly(params)
-        svg = File()  # temporary file to store 2D schematic as svg
+        svg = vkt.File()  # temporary file to store 2D schematic as svg
         cq.exporters.export(body, svg.source, exportType=cq.exporters.ExportTypes.SVG)
-        return ImageResult(svg)
+        return vkt.ImageResult(svg)
